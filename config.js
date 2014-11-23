129d5b0c92341be8215addaeba922f873b42c7db
Daniel Moran <dmj@tid.es>
2014-11-23 23:53:45 +0100

    ADD Initial Commit

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {};
+
+config.server = {
+    host: 'localhost',
+    port: 60001
+}
+
+config.client = {
+    endpoint: 'lwm2m-arduino',
+    port: 5683,
+    lifetime: '85671',
+    version: '1.0',
+    observe: {
+        period: 3000
+    }
+}
+
+module.exports = config;
---
+++ lwm2m-arduino.js
@@ -0,0 +1,71 @@
+var five = require("johnny-five"),
+    lwm2mClient = require('iotagent-lwm2m-lib').client,
+    async = require('async'),
+    apply = async.apply,
+    config = require('./config'),
+    globalDeviceInfo,
+    board = new five.Board(),
+    mapping = {
+        objectType: 5,
+        objectInstance: 1
+    };
+
+
+function handleWrite(objectType, objectId, resourceId, value, callback) {
+    console.log('Value written:\n--------------------------------\n');
+    console.log('-> ObjectType: %s', objectType);
+    console.log('-> ObjectId: %s', objectId);
+    console.log('-> ResourceId: %s', resourceId);
+    console.log('-> Written value: %s', value);
+
+    callback(null);
+}
+
+function handleRead(objectType, objectId, resourceId, value, callback) {
+    console.log('Value read:\n--------------------------------\n');
+    console.log('-> ObjectType: %s', objectType);
+    console.log('-> ObjectId: %s', objectId);
+    console.log('-> ResourceId: %s', resourceId);
+    console.log('-> Read Value: %s', value);
+
+    callback(null, value);
+}
+
+function setHandlers(deviceInfo) {
+    lwm2mClient.setHandler(deviceInfo.serverInfo, 'write', handleWrite);
+    lwm2mClient.setHandler(deviceInfo.serverInfo, 'read', handleRead);
+}
+
+function connect(config, callback) {
+    console.log('Trying to connect to Lightweight M2M Server');
+    lwm2mClient.register(config.server.host, config.server.port, config.client.endpoint, function (error, deviceInfo) {
+        if (error) {
+            console.log('Failed to connect');
+            callback(error);
+        } else {
+            setHandlers(deviceInfo);
+            globalDeviceInfo = deviceInfo;
+            callback(null, deviceInfo);
+        }
+    });
+}
+
+board.on('ready', function() {
+    var objUri = '/' + mapping.objectType + '/' + mapping.objectInstance,
+        connectionFlow = [
+            apply(connect, config),
+            apply(lwm2mClient.registry.create, objUri)
+        ];
+
+    for (var i = 0; i < 13; i++) {
+        connectionFlow.push(apply(lwm2mClient.registry.setAttribute, objUri, i, 0));
+    }
+
+    async.series(connectionFlow, function (error) {
+        if (error) {
+            console.log('There was an error starting the server');
+        } else {
+            console.log('Server started succesfully');
+        }
+    });
+});
